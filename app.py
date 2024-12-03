4d9746524f99bbf81492ec237ad2ac3b493530fc
shivam <shivamstk7777@gmail.com>
2024-12-03 16:51:27 +0530

    add starup_cleaned csv file

--- app.py
+++ app.py
@@ -4,11 +4,25 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(layout='wide',page_title='StartUp Analysis',page_icon="❤️",initial_sidebar_state="expanded")
 
+def load_overall_analysis():
+    st.write('Johny Johny Yes Papa')
+
+def load_investor_analysis():
+    st.subheader('Startup analysis')
+
+def load_startup_analysis():
+    st.header('Startup analysis')
+
+
 st.sidebar.title('Startup Funding Analysis')
-st.sidebar.selectbox('Select one',['Overall Analysis','Investor','StartUp'])
+option = st.sidebar.selectbox('Select one',['Overall Analysis','Investor','StartUp'])
 
-
-
+if option == 'Overall Analysis':
+    load_overall_analysis()
+elif option == 'Investor':
+    st.selectbox()
+else:
+    load_startup_analysis()
 
 
 
